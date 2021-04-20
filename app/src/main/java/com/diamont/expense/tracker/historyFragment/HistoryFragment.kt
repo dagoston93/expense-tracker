8ea3dc9fcf5b339a9e7915885bbc611d039d7cfd
Agoston <dagoston93@gmail.com>
2021-04-20 11:50:21 +0200

    User selected date range filtering on HistoryFragment implemented.

--- app/src/main/java/com/diamont/expense/tracker/historyFragment/HistoryFragment.kt
+++ app/src/main/java/com/diamont/expense/tracker/historyFragment/HistoryFragment.kt
@@ -1,10 +1,14 @@
 package com.diamont.expense.tracker.historyFragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.core.util.Pair
+import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -17,6 +21,8 @@ import com.diamont.expense.tracker.util.Currency
 import com.diamont.expense.tracker.util.KEY_PREF_CURRENCY_ID
 import com.diamont.expense.tracker.util.database.*
 import com.diamont.expense.tracker.util.enums.TransactionType
+import com.diamont.expense.tracker.util.getStringListIndexFromText
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.text.DecimalFormat
 
@@ -24,6 +30,13 @@ class HistoryFragment : Fragment() {
     /** Data binding */
     private lateinit var binding : FragmentHistoryBinding
     private lateinit var viewModel: HistoryFragmentViewModel
+
+    /** Array adapter for period list */
+    private lateinit var periodAdapter : ArrayAdapter<String>
+    private var previousSelectedIndex: Int? = 0
+    private var previousSelectedStartDate: Long = 0
+    private var previousSelectedEndDate: Long = 0
+
 
     /** Get the Activity View Model */
     private val activityViewModel : MainActivityViewModel by activityViewModels {
@@ -83,12 +96,73 @@ class HistoryFragment : Fragment() {
         /** Turn the blinking animation on item change off */
         binding.rvTransactionList.itemAnimator = null
 
+        /**
+         * Add text changed listener for the period dropdown
+         */
+        binding.actvHistoryInterval.addTextChangedListener {
+            val idx = binding.actvHistoryInterval.getStringListIndexFromText(viewModel.periodStringList.value ?: listOf<String>())
+
+            if(idx != previousSelectedIndex) {
+
+                if (idx == viewModel.periodStringList.value?.size!! - 1) {
+                    /**
+                     * Create the date picker
+                     */
+                    val datePickerBuilder =
+                        MaterialDatePicker.Builder.dateRangePicker()
+                            .setTitleText(resources.getString(R.string.select_period))
+
+                    /**
+                     * If user has selected a date range before, we remember the selection
+                     */
+                    if(previousSelectedStartDate != 0L && previousSelectedEndDate != 0L){
+                        datePickerBuilder.setSelection(
+                            Pair(
+                                previousSelectedStartDate,
+                                previousSelectedEndDate
+                            )
+                        )
+                    }
+
+                    val datePicker = datePickerBuilder.build()
+
+                    /**
+                     * OnClickListener for the date picker OK button
+                     */
+                    datePicker.addOnPositiveButtonClickListener {
+                        viewModel.onDateRangeSelected(it.first, it.second)
+                        previousSelectedStartDate = it.first ?: 0
+                        previousSelectedEndDate = it.second ?: 0
+
+                        val rangeString = "${formatDate(previousSelectedStartDate)} - ${formatDate(previousSelectedEndDate)}"
+                        binding.actvHistoryInterval.setText(rangeString, false)
+                    }
+
+                    datePicker.addOnCancelListener {
+                        resetSelection()
+                    }
+
+                    datePicker.addOnNegativeButtonClickListener {
+                        resetSelection()
+                    }
+
+                    datePicker.show(childFragmentManager, "")
+
+                    Log.d("GUS", "Show date range picker...")
+                } else {
+                    previousSelectedIndex = idx
+                    viewModel.onPeriodDropdownItemSelected(idx)
+                }
+            }
+        }
+
         /** Observe the data and refresh recycler view if it changes */
-        viewModel.transactionData.observe(viewLifecycleOwner, Observer {
+        viewModel.transactionDataToDisplay.observe(viewLifecycleOwner, Observer {
             it?.let{
                 adapter.categories = viewModel.categories.value ?: listOf<TransactionCategory>()
                 adapter.plans = viewModel.plans.value ?: listOf<Plan>()
                 adapter.items = it.toMutableList()
+                Log.d("GUS", "new list...")
             }
         })
 
@@ -101,6 +175,15 @@ class HistoryFragment : Fragment() {
                 activityViewModel.eventNavigateToEditFragment.value = it
                 viewModel.eventNavigateToEditFragment.value = null
             }
+        })
+
+        /**
+         * Observe period string list
+         */
+        viewModel.periodStringList.observe(viewLifecycleOwner, Observer {
+            periodAdapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, it)
+            binding.actvHistoryInterval.setAdapter(periodAdapter)
+            binding.actvHistoryInterval.setText(periodAdapter.getItem(0).toString(), false)
         })
 
         /** Return the inflated layout */
@@ -124,6 +207,39 @@ class HistoryFragment : Fragment() {
                 (binding.rvTransactionList.adapter as TransactionRecyclerViewAdapter).itemDeletedAtPos(position)
             }
             .show()
+    }
+
+    /**
+     * Call this method if material date picker was cancelled
+     */
+    private fun resetSelection(){
+        /** If previous index is not null we need to select that item*/
+        if(previousSelectedIndex!= null) {
+            binding.actvHistoryInterval.setText(periodAdapter.getItem(previousSelectedIndex!!).toString(), false)
+        }else{
+            /** If it is null, we need to set the text to the prev. date range */
+            val rangeString = "${formatDate(previousSelectedStartDate)} - ${formatDate(previousSelectedEndDate)}"
+            binding.actvHistoryInterval.setText(rangeString, false)
+        }
+    }
+
+    /**
+     * Call this method to format a date
+     */
+    private fun formatDate(date: Long): String{
+        val dateFormat = android.text.format.DateFormat.getDateFormat(context)
+        return dateFormat.format(date)
+    }
+
+    /**
+     * We fill the dropdown menus in onResume() so that
+     * if device configuration is changed we don't loose the
+     * items from the menu
+     */
+    override fun onResume() {
+        binding.actvHistoryInterval.setAdapter(periodAdapter)
+
+        super.onResume()
     }
 
 }
--- app/src/main/java/com/diamont/expense/tracker/historyFragment/HistoryFragmentViewModel.kt
+++ app/src/main/java/com/diamont/expense/tracker/historyFragment/HistoryFragmentViewModel.kt
@@ -7,14 +7,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.diamont.expense.tracker.R
 import com.diamont.expense.tracker.util.database.Plan
-import com.diamont.expense.tracker.util.enums.TransactionFrequency
-import com.diamont.expense.tracker.util.enums.TransactionType
 import com.diamont.expense.tracker.util.database.Transaction
 import com.diamont.expense.tracker.util.database.TransactionCategory
 import com.diamont.expense.tracker.util.database.TransactionDatabaseDao
-import com.diamont.expense.tracker.util.enums.PaymentMethod
-import com.diamont.expense.tracker.util.enums.TransactionPlanned
+import com.diamont.expense.tracker.util.enums.TransactionType
 import kotlinx.coroutines.*
+import java.util.*
 
 class HistoryFragmentViewModel(
     private val appContext: Application,
@@ -23,9 +21,9 @@ class HistoryFragmentViewModel(
     /**
      * Set up some live data
      */
-    private val _transactionData = MutableLiveData<List<Transaction>>()
-    val transactionData : LiveData<List<Transaction>>
-        get() = _transactionData
+    private val _transactionDataToDisplay = MutableLiveData<List<Transaction>>()
+    val transactionDataToDisplay : LiveData<List<Transaction>>
+        get() = _transactionDataToDisplay
 
     private val _categories = MutableLiveData<List<TransactionCategory>>()
     val categories : LiveData<List<TransactionCategory>>
@@ -34,6 +32,22 @@ class HistoryFragmentViewModel(
     private val _plans = MutableLiveData<List<Plan>>()
     val plans : LiveData<List<Plan>>
         get() = _plans
+
+    private val _periodStringList = MutableLiveData<List<String>>(listOf<String>())
+    val periodStringList : LiveData<List<String>>
+        get() = _periodStringList
+
+    /**
+     * Declare some variables
+     */
+    private var transactionData : List<Transaction> = listOf<Transaction>()
+    private var filteredTransactionData : List<Transaction> = mutableListOf<Transaction>()
+    private var selectedIndex: Int? = 0
+    private var calendarStartDate = Calendar.getInstance()
+    private var calendarEndDate = Calendar.getInstance()
+
+    private var filterTransactionTypes: MutableList<TransactionType> = mutableListOf()
+    private var filterCategoryIds: MutableList<Int> = mutableListOf()
 
     /**
      * Trigger this event when user clicks on an edit icon
@@ -54,6 +68,89 @@ class HistoryFragmentViewModel(
      */
     init{
         getTransactionData()
+
+        /** Set up the array adapter */
+        _periodStringList.value = listOf(
+            appContext.resources.getString(R.string.all_transactions),
+            appContext.resources.getString(R.string.current_month),
+            appContext.resources.getString(R.string.last_seven_days),
+            appContext.resources.getString(R.string.previous_month),
+            appContext.resources.getString(R.string.this_year),
+            appContext.resources.getString(R.string.select_period)
+        )
+    }
+
+    companion object{
+        const val IDX_ALL_TRANSACTIONS: Int = 0
+        const val IDX_CURRENT_MONTH: Int = 1
+        const val IDX_LAST_SEVEN_DAYS: Int = 2
+        const val IDX_PREVIOUS_MONTH: Int = 3
+        const val IDX_THIS_YEAR: Int = 4
+        const val IDX_DATE_RANGE: Int = 5
+    }
+
+    /**
+     * Call this method if user selects an option from the dropdown menu
+     */
+    fun onPeriodDropdownItemSelected(index: Int?){
+        Log.d("GUS", "item: $index")
+
+        if(index != null){
+            selectedIndex = index
+            filterTransactionList()
+        }else{
+            selectedIndex = IDX_DATE_RANGE
+        }
+    }
+
+    /**
+     * Call this method if user selects a date range
+     */
+    fun onDateRangeSelected(startDate: Long?, endDate: Long?){
+        Log.d("GUS", "date range picked: $startDate, $endDate")
+
+        if(startDate != null && endDate != null) {
+            calendarStartDate.timeInMillis = startDate
+            calendarEndDate.timeInMillis = endDate
+        }
+
+        selectedIndex = IDX_DATE_RANGE
+        filterTransactionList()
+    }
+
+    /**
+     * Call this method to filter the list
+     */
+    private fun filterTransactionList(){
+        Log.d("GUS", "filtering...")
+        filteredTransactionData = transactionData.filter{
+            var isItemDisplayed = true
+
+            /** Check if item is within date range */
+            if(selectedIndex != IDX_ALL_TRANSACTIONS) {
+                if (it.date !in calendarStartDate.timeInMillis..calendarEndDate.timeInMillis) {
+                    isItemDisplayed = false
+                }
+            }
+
+            /** Check the category filters */
+            if(filterCategoryIds.isNotEmpty()){
+                if(!filterCategoryIds.contains(it.categoryId)){
+                    isItemDisplayed = false
+                }
+            }
+
+            /** Check transaction type filters */
+            if(filterTransactionTypes.isNotEmpty()){
+                if(!filterTransactionTypes.contains(it.transactionType)){
+                    isItemDisplayed = false
+                }
+            }
+
+            isItemDisplayed
+        }
+
+        _transactionDataToDisplay.value = filteredTransactionData
     }
 
     /**
@@ -63,7 +160,8 @@ class HistoryFragmentViewModel(
         uiScope.launch {
             _categories.value = databaseDao.getCategoriesSuspend()
             _plans.value = databaseDao.getAllPlansSuspend()
-            _transactionData.value = databaseDao.getAllTransactionsSuspend()
+            transactionData = databaseDao.getAllTransactionsSuspend()
+            _transactionDataToDisplay.value = transactionData
         }
     }
 
